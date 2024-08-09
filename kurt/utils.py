857c5bacba588b46248e7d077267e0fc57364ed6
Alex Ruco <alex@ruco.pt>
2024-08-09 16:12:47 +0100

    created non-content extensions on utils.py

--- kurt/utils.py
+++ kurt/utils.py
@@ -1,2 +1,17 @@
-from urllib.parse import urlparse
 
+# kurt/utils.py
+
+def no_content_extensions():
+    """
+    Returns a list of file extensions that typically represent non-HTML content.
+    """
+    return [
+        'jpg', 'jpeg', 'png', 'gif', 'webp', 'bmp', 'svg',
+        'mp4', 'mov', 'avi', 'mkv', 'flv', 'wmv',
+        'mp3', 'wav', 'ogg', 'flac', 'aac',
+        'pdf', 'doc', 'docx', 'xls', 'xlsx', 'ppt', 'pptx',
+        'zip', 'rar', '7z', 'tar', 'gz',
+        'exe', 'msi', 'dmg',
+        'iso', 'img',
+        'txt', 'csv', 'json', 'xml'
+    ]
